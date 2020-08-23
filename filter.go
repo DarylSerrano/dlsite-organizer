158c6dc921ad1276d7f63de5b9b0c22e6b0e3832
DarylSerrano <darylserrano39@gmail.com>
2020-08-23 13:26:39 +0200

    Add filter.go with functions to be implemented

---
+++ filter.go
@@ -0,0 +1,19 @@
+package main
+
+import "database/sql"
+
+func createSymlink(filepath string) {
+
+}
+
+func filterByCircle(db *sql.DB, circleID int) {
+
+}
+
+func filterByVoiceActor(db *sql.DB, voiceActorID int) {
+
+}
+
+func filterByTag(db *sql.DB, tagName string) {
+
+}
