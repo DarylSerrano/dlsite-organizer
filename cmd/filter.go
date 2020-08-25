5f2f6040dfcf87f330e78671c2867931d6e3c571
Daryl <darylserrano39@gmail.com>
2020-08-25 18:02:12 +0200

    Add filterSWF subcommand

--- cmd/filter.go
+++ cmd/filter.go
@@ -11,6 +11,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var isSfw bool
+
 var cmdRootFilter = &cobra.Command{
 	Use:   "filter",
 	Short: "Filter works, default filter by sfw",
@@ -37,8 +39,34 @@ var cmdRootFilter = &cobra.Command{
 	},
 }
 
-var cmdSfwFilter = &cobra.Command{}
+var cmdSfwFilter = &cobra.Command{
+	Use:   "sfw",
+	Short: "Filter work by sfw/nsfw",
+	Long:  "Filter work by sfw/nsfw",
+	Args:  cobra.MaximumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		basePath, err := getBasePath(args)
+		if err != nil {
+			log.Fatal(err)
+		}
+		databasePath := filehandler.CreateDBFile(dbDir)
+
+		fmt.Println("BasePath: " + *basePath)
+		fmt.Println("Databasepath: " + dbDir)
+
+		db, err := database.OpenDB(databasePath)
+		defer db.Close()
+		if err != nil {
+			fmt.Print(err)
+			os.Exit(1)
+		}
+
+		filter.FilterBySfw(db, isSfw, *basePath)
+	},
+}
 
 func init() {
+	cmdSfwFilter.Flags().BoolVarP(&isSfw, "isSFW", "s", true, "Shoild filter by SFW Works")
+	cmdRootFilter.AddCommand(cmdSfwFilter)
 	rootCmd.AddCommand(cmdRootFilter)
 }
